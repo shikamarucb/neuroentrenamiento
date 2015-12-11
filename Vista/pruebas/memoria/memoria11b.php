6761acdab045eb6c8fd3cc7daea6f7f2a645db8f
Andres Camacho <nescabezadepez12@hotmail.com>
2015-12-11 15:43:44 -0500

    Añadida prueba de memoria
    Se añade prueba de memoria de la semana 1 dia 1.

---
+++ Vista/pruebas/memoria/memoria11b.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="../../main.css" rel="stylesheet" type="text/css">
+    <link href="memoria.css" rel="stylesheet" type="text/css">
+
+    <link href='https://fonts.googleapis.com/css?family=Poiret+One|Jura:600,400,500|Exo+2:400,500' rel='stylesheet' type='text/css'>
+    <title>Plantilla</title>
+</head>
+<body>
+<nav id="navi">
+    <div id="title">
+        <h1>Neuroentrenamiento</h1>
+    </div>
+    <div id="menu">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+            <li><a href="register.html">Registrarse</a></li>
+            <li><a href="login.html">Iniciar Sesion</a></li>
+        </ul>
+    </div>
+</nav>
+    
+<div id="contmem">
+    <div><br><br>
+        <p>Clasifica las palabras anteriores de acuerdo a su categoría y anótalas en el siguiente cuadro. Recuerda que no puedes observar la página anterior.</p><br><br><br>
+        <form>
+            <table>
+                <tr>
+                    <th>Herramientas</th>
+                    <th>Frutas</th>
+                </tr>
+                <tr>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                </tr>
+            </table><br><br><br><br>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</div>
+<footer>
+    <div id="footerP">
+        <br><br><div>
+            <p>Universidad de Cundinamarca &copy;</p>
+        </div>
+        <div>
+            <p>Centro de investigacion tecnológica</p>
+        </div>
+        <div>
+            <p>2016</p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
