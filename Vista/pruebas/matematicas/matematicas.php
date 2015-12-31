0a68fbee13600dbdd9d1a31be04e7a353ba40fba
Andres Camacho <nescabezadepez12@hotmail.com>
2015-12-31 00:30:03 -0500

    Nuevas pruebas
    Se añade una nueva prueba de memoria semana 1 dia 2; se añade
    plantilla para la mayoria de las pruebas de matematicas.

---
+++ Vista/pruebas/matematicas/matematicas.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="../../main.css" rel="stylesheet" type="text/css">
+    <link href="mate.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Poiret+One|Jura:600,400,500|Exo+2:400,500' rel='stylesheet' type='text/css'>
+    <title>Plantilla</title>
+</head>
+<body>
+<nav id="navi">
+    <div id="title">
+        <h1>Neuroentrenamiento</h1>
+    </div>
+    <div id="menu">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+            <li><a href="register.html">Registrarse</a></li>
+            <li><a href="login.html">Iniciar Sesion</a></li>
+        </ul>
+    </div>
+</nav>
+    
+<div id="contMat" style="height:680px;">
+    <div id="contMat2">
+        <h1>¡PRACTICA CON NÚMEROS! </h1><br><br>
+        <p>A continuación se presentan una serie de ejercicios matemáticos que debes resolver. Cuando en un ejercicio encuentres paréntesis, primero desarrolla las operaciones que se encuentren dentro de ellos. </p><br><br>
+        <form style="text-align:center;">
+            <table id="tableMat">
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+                <tr>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                    <td><div class="contPr"><label>1+4+5+6+7+9=</label><input type="number"></div></td>
+                </tr>
+            </table>
+            <button id="subBA" type="submit">Enviar</button>
+        </form>
+        
+    </div>
+</div>
+<footer>
+    <div id="footerP">
+        <br><br><div>
+            <p>Universidad de Cundinamarca &copy;</p>
+        </div>
+        <div>
+            <p>Centro de investigacion tecnológica</p>
+        </div>
+        <div>
+            <p>2016</p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
