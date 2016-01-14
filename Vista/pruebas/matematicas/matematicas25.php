51598e0d856720bc3b5f9aaa94f9c1184e177793
Andres Camacho <nescabezadepez12@hotmail.com>
2016-01-13 19:19:01 -0500

    Nuevas pruebas de matematicas
    Se añaden las demas pruebas de matematicas

---
+++ Vista/pruebas/matematicas/matematicas25.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="../../main.css" rel="stylesheet" type="text/css">
+    <link href="mate.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Poiret+One|Jura:600,400,500|Exo+2:400,500' rel='stylesheet' type='text/css'>
+    <title>Plantilla</title>
+</head>
+<body>
+<nav id="navi">
+    <div id="title">
+        <h1>Neuroentrenamiento</h1>
+    </div>
+    <div id="menu">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+            <li><a href="register.html">Registrarse</a></li>
+            <li><a href="login.html">Iniciar Sesion</a></li>
+        </ul>
+    </div>
+</nav>
+    
+<div id="contMat" style="height:540px;">
+    <div id="contMat2">
+        <h1>¡PRACTICA CON NÚMEROS! </h1><br><br>
+        <p>A continuación se presentan dos columnas, una con operaciones aritméticas (izquierda) y otra con sus soluciones (derecha). Coloca el número del enunciado correspondiente al resultado.</p><br><br>
+        <form style="text-align:center;">
+            <div class="contPr2"><pre>1) 6-3                                            16  <input type="number"></pre></div><br>
+            <div class="contPr2"><pre>2) 12-6                                          3    <input type="number"></pre></div><br>
+            <div class="contPr2"><pre>3) 34/2                                          14   <input type="number"></pre></div><br>
+            <div class="contPr2"><pre>4) (20-11)+5                                  6    <input type="number"></pre></div><br>
+            <div class="contPr2"><pre>5) (16/4)x(16/4)                             4    <input type="number"></pre></div><br>
+            <div class="contPr2"><pre>6) (2x1)x2                                      17  <input type="number"></pre></div><br>
+            <button id="subBA" type="submit">Enviar</button>
+        </form>
+        
+    </div>
+</div>
+<footer>
+    <div id="footerP">
+        <br><br><div>
+            <p>Universidad de Cundinamarca &copy;</p>
+        </div>
+        <div>
+            <p>Centro de investigacion tecnológica</p>
+        </div>
+        <div>
+            <p>2016</p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ Vista/pruebas/matematicas/matematicas33.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="../../main.css" rel="stylesheet" type="text/css">
+    <link href="mate.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Poiret+One|Jura:600,400,500|Exo+2:400,500' rel='stylesheet' type='text/css'>
+    <title>Plantilla</title>
+</head>
+<body>
+<nav id="navi">
+    <div id="title">
+        <h1>Neuroentrenamiento</h1>
+    </div>
+    <div id="menu">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+            <li><a href="register.html">Registrarse</a></li>
+            <li><a href="login.html">Iniciar Sesion</a></li>
+        </ul>
+    </div>
+</nav>
+    
+<div id="contMat" style="height:640px;">
+    <div id="contMat2">
+        <h1>¡PRACTICA CON NÚMEROS! </h1><br><br>
+        <p>A continuación se presentan una serie de ejercicios matemáticos que debes resolver. Cuando en un ejercicio encuentres paréntesis, primero desarrolla las operaciones que se encuentren dentro de ellos. </p><br><br>
+        <form style="text-align:center;">
+            <div class="contPr3"><pre>1) (56 + 34) – <input type="number"> = 60    </pre></div><br>
+            <div class="contPr3"><pre>2) (<input type="number"> - 78) ÷ 3 = 4</pre></div><br>
+            <div class="contPr3"><pre>3) (68 ÷ <input type="number">) + 5 = 39</pre></div><br>
+            <div class="contPr3"><pre>4) (22 + <input type="number">) + 8 = 50</pre></div><br>
+            <div class="contPr3"><pre>5) (<input type="number"> + 32) – 30 = 82</pre></div><br>
+            <div class="contPr3"><pre>6) (40 ÷ <input type="number">) + 12 = 22</pre></div><br>
+            <div class="contPr3"><pre>7) (83 – 33) + <input type="number"> = 7</pre></div><br>
+            <div class="contPr3"><pre>8) 17 + (44 - <input type="number">) = 21</pre></div><br>
+            <div class="contPr3"><pre>9) (<input type="number"> + 54) ÷ 2 = 28</pre></div><br>
+            <div class="contPr3"><pre>10) (34 + 16) - <input type="number"> = 25</pre></div><br>
+            <button id="subBA" type="submit">Enviar</button>
+        </form>
+        
+    </div>
+</div>
+<footer>
+    <div id="footerP">
+        <br><br><div>
+            <p>Universidad de Cundinamarca &copy;</p>
+        </div>
+        <div>
+            <p>Centro de investigacion tecnológica</p>
+        </div>
+        <div>
+            <p>2016</p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
